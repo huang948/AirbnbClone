9f86d432fa06f731cf810ddcb2c2ea944ff3915c
jordan huang <jordanhuang99@gmail.com>
2020-12-31 23:41:16 +0100

    Post props

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,38 +2,30 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <View style={styles.container}>
       {/* Image */}
-      <Image
-        style={styles.image}
-        source={{
-          uri:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-        }}
-      />
+      <Image style={styles.image} source={{uri: post.image}} />
 
       {/* Bed & Bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={2}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni optio
-        iure earum beatae saepe quia eos ipsa dolorum tempore quibusdam.
-        Cupiditate maxime hic, amet deleniti rerum libero. Commodi, repellat
-        unde!
+        {post.type}. {post.title}
       </Text>
 
       {/* Old price & new price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.price}>  $30 </Text>
-        / night
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.price}> ${post.newPrice} </Text>/ night
       </Text>
 
       {/* Total price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
