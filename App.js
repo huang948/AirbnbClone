c9bb4ca90645a269c354232f51868f75749ee388
jordan huang <jordanhuang99@gmail.com>
2020-12-31 23:57:33 +0100

    List of props

--- App.js
+++ App.js
@@ -10,11 +10,13 @@ import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
+import SearchResultsScreen from './src/screens/SearchResults';
 import Post from './src/components/Post';
 
 import feed from './assets/data/feed';
 
-const post1 = feed[2]; 
+const post1 = feed[0];
+const post2 = feed[2]; 
 
 const App = () => {
   return (
@@ -22,7 +24,8 @@ const App = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post post={post1} />
+        {/* <Post post={post2} /> */}
+        <SearchResultsScreen />
       </SafeAreaView>
     </>
   );
