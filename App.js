3e290dd8edea72bb8e15b7713160b52a43e78aff
jordan huang <jordanhuang99@gmail.com>
2021-02-28 23:35:16 -0700

    added amplify

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {StatusBar} from 'react-native';
 
 import Router from './src/navigation/Router';
 
-import { withAuthenticator } from 'aws-amplify-react-native';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
 const App = () => {
   return (
@@ -24,3 +24,5 @@ const App = () => {
 };
 
 export default withAuthenticator(App);
+
+// TIME 1:00:00
